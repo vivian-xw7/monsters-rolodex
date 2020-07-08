be2330aec7b8b4c6b83029f71197ec938d8f3005
Vivian Hayes <viv.hayes@yahoo.com>
2020-07-07 20:44:58 -0500

    monsters rolodex to github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import CardList from "./components/card-list/CardList";
 import './App.css';
+import { SearchBox } from "./components/search-box/search-box";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  // React promises. The list is already provided by something else, and we fetch it in the URL.
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
+  }
+
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value});
+  }
+
+  render () {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+    return (
+      <div className='App'>
+        <h1> Monsters Rolodex </h1>
+        {/* The e is the synthetic event, the target is where on the page it happened, the value is the string value. */}
+        <SearchBox
+          placeholder='Search Monsters'
+          handleChange={this.handleChange}
+        />
+        {/* children are what's passed between a component */}
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/card-list/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./card.css";
+
+const Card = props => (
+    <div className="card-container">
+    <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=150x150`} />
+        <h2>{props.monster.name}</h2>
+        <p>{props.monster.email}</p>
+    </div>
+);
+
+export default Card;
