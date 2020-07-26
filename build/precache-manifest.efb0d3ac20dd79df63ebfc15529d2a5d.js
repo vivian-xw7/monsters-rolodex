5bfe19ffbcd404ef0aa116f6ba22fee8db276035
Vivian Hayes <viv.hayes@yahoo.com>
2020-07-26 11:21:44 -0500

    adding files to github

---
+++ build/precache-manifest.efb0d3ac20dd79df63ebfc15529d2a5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30867b963994ec9877bd3c10a1be7ea6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ee86b7ddf08c7a5454ec",
+    "url": "/monsters-rolodex/static/css/main.1412fe52.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee86b7ddf08c7a5454ec",
+    "url": "/monsters-rolodex/static/js/main.cfce8472.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
